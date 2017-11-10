849b62f457661c4c33d1f86f0bdbdfbe600d5dc4
luoxing <luoxing@didichuxing.com>
2017-11-10 18:06:18 +0800

    using LLVMTarget

--- ast.hpp
+++ ast.hpp
@@ -1,18 +1,69 @@
 #include <string>
 #include <vector>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/IRBuilder.h>
+
 class ExprAST{
 public:
     virtual ~ExprAST(){}
-    virtual Value *codegen() = 0;
+    virtual llvm::Value *codegen() = 0;
+};
+static llvm::LLVMContext TheContext;
+static std::unique_ptr<llvm::Module> TheModule;
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+public:
+    PrototypeAST(const std::string &name, std::vector<std::string> Args)
+            :Name(name), Args(std::move(Args)){}
+    inline const std::string & getName() const {return Name;}
+    llvm::Function* codegen(){
+        std::vector<llvm::Type *> Doubles(Args.size(), llvm::Type::getDoubleTy(TheContext));
+        llvm::FunctionType *FT =
+                llvm::FunctionType::get(llvm::Type::getDoubleTy(TheContext), Doubles, false);
+        llvm::Function *F =
+                llvm::Function::Create(FT, llvm::Function::ExternalLinkage, Name, TheModule.get());
+
+        // Set names for all arguments.
+        unsigned Idx = 0;
+        for (auto &Arg : F->args()){
+            Arg.setName(Args[Idx++]);
+        }
+        
+        return F;
+        
+    }
+        
 };
 
-static LLVMContext TheContext;
-static IRBuilder<> Builder(TheContext);
-static std::unique_ptr<Module> TheModule;
-static std::map<std::string, Value *> NamedValues;
 
-Value *LogErrorV(const char *Str) {
+
+static llvm::IRBuilder<> Builder(TheContext);
+
+static std::map<std::string, llvm::Value *> NamedValues;
+static std::map<std::string, std::unique_ptr<PrototypeAST>> FunctionProtos;
+
+llvm::Function *getFunction(std::string Name) {
+    if (auto *F = TheModule->getFunction(Name)){
+        return F;
+    }
+    // if not , checker
+    auto FI = FunctionProtos.find(Name);
+    if (FI != FunctionProtos.end()){
+        return FI->second->codegen();
+    }
+    return nullptr;
+        
+}
+
+
+std::unique_ptr<ExprAST> LogError(const char *Str) {
+  fprintf(stderr, "Error: %s\n", Str);
+  return nullptr;
+}
+
+llvm::Value *LogErrorV(const char *Str) {
   LogError(Str);
   return nullptr;
 }
@@ -21,8 +72,8 @@ class NumberExprAST : public ExprAST {
     double Val;
 public:
     NumberExprAST(double Val) : Val(Val){}
-    Value *NumberExprAST::codegen() {
-        return ConstantFP::get(TheContext, APFloat(Val));
+    llvm::Value *codegen() {
+        return llvm::ConstantFP::get(TheContext, llvm::APFloat(Val));
     }
 };
 
@@ -30,13 +81,14 @@ class VariableExprAST : public ExprAST {
     std::string Name;
 public:
     VariableExprAST(const std::string &Name) : Name(Name) {}
-    Value *VariableExprAST::codegen() {
+    llvm::Value *codegen() {
   // Look this variable up in the function.
-  Value *V = NamedValues[Name];
-  if (!V)
-    LogErrorV("Unknown variable name");
-  return V;
-}
+        llvm::Value *V = NamedValues[Name];
+        if (!V){
+            return LogErrorV("Unknown variable name");
+        }
+        return V;
+    }
 };
 
 
@@ -48,22 +100,23 @@ public:
     BinaryExprAST(char op, std::unique_ptr<ExprAST> left,
                   std::unique_ptr<ExprAST> right):
             op(op), left(std::move(left)), right(std::move(right)){}
-    Value *BinaryExprAST::codegen(){
-        Value* left = left->codegen();
-        Value* right = right->codegen();
+    llvm::Value *codegen() override{
+        llvm::Value* leftValue = left->codegen();
+        llvm::Value* rightValue = right->codegen();
         if(left == NULL || right == NULL){
             return nullptr;
         }
         switch(op){
         case '+':
-            return Builder.CreateFAdd(left, right, "addtmp");
+            return Builder.CreateFAdd(leftValue, rightValue, "addtmp");
         case '-':
-            return Builder.CreateFSub(left, right, "subtmp");
+            return Builder.CreateFSub(leftValue, rightValue, "subtmp");
         case '*':
-            return Builder.CreateFMul(left, right, "multmp");
+            return Builder.CreateFMul(leftValue, rightValue, "multmp");
         case '<':
-            left = Builder.CreateFCmpULT(left, right, "cmptmp");
-            return Builder.CreateUIToFP(left, Type::getDoubleTy(TheContext), "booltmp");
+            leftValue = Builder.CreateFCmpULT(leftValue, rightValue, "cmptmp");
+            return Builder.CreateUIToFP(leftValue,
+                                        llvm::Type::getDoubleTy(TheContext), "booltmp");
         default:
             return LogErrorV("invalid binary operator");
         }
@@ -71,14 +124,6 @@ public:
 };
 
 
-class PrototypeAST {
-    std::string Name;
-    std::vector<std::string> Args;
-public:
-    PrototypeAST(const std::string &name, std::vector<std::string> Args)
-            :Name(name), Args(std::move(Args)){}
-    inline const std::string & getName() const {return Name;}
-};
 
 
 
@@ -91,6 +136,23 @@ public:
     CallExprAST(const std::string& Callee,
                 std::vector<std::unique_ptr<ExprAST>> Args) 
         :Callee(Callee), Args(std::move(Args)){}
+    llvm::Value* codegen(){
+        llvm::Function *CalleeF = getFunction(Callee);
+        if (!CalleeF){
+            return LogErrorV("Unknown function referenced");
+        }
+        if(CalleeF->arg_size() != Args.size()){
+            return LogErrorV("Incorrect # arguments passed");
+        }
+        std::vector<llvm::Value *> ArgsV;
+        for (unsigned i = 0, e = Args.size(); i != e; ++i) {
+            ArgsV.push_back(Args[i]->codegen());
+            if (!ArgsV.back()){
+                return nullptr;
+            }
+        }
+        return Builder.CreateCall(CalleeF, ArgsV, "calltmp");
+    }
 };
 
 class FunctionAST {
